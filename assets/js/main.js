dd3b24cab6f8ad4958eb8c12bfd5c54e9e44667d
Nguyen Hoang Viet <vietdota12@gmail.com>
2023-08-15 21:31:37 +0700

    Complete Repeat/Back/Forward/Random Event, Fixed UI

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,20 +4,34 @@ const $$=document.querySelectorAll.bind(document);
 
 function secondstoMinutesandSeconds(seconds){
     let minutes=Math.floor(seconds/60);
-    let leftSeconds=(seconds%60).toFixed(0);
+    let leftSeconds=Math.ceil(seconds%60);
     return minutes + ':' + (leftSeconds < 10 ? '0':'') + leftSeconds;
 }
 
-const player=$('.player');
-const playList=$('.playlist');
-const audio=$('#audio');
-const headerNameSong=$('.header h2');
-const headerAuthor=$('.header p');
-const cdThumb=$('.cd-thumb');
-const toggleAudio=$('.btn.btn-toggle-play');
-const playBarCurrent=$('.play-bar .play-current');
-const playBarLength=$('.play-bar .play-length');
-const progress=$('#progress');
+const player = $('.player');
+const playList = $('.playlist');
+const audio = $('#audio');
+const headerNameSong = $('.header h2');
+const headerAuthor = $('.header p');
+const cdThumb = $('.cd-thumb');
+const toggleAudio = $('.btn.btn-toggle-play');
+const repeatAudio = $('.btn.btn-repeat');
+const preAudio = $('.btn.btn-pre');
+const nextAudio = $('.btn.btn-next');
+const randomAudio = $('.btn.btn-random');
+const cdThumbAnimate = cdThumb.animate([{
+    transform: "rotate(0)"
+    },
+    {
+    transform: "rotate(360deg)"}],{
+        duration:15000,
+        iterations:Infinity
+    }
+);
+
+const playBarCurrent = $('.play-bar .play-current');
+const playBarLength =   $('.play-bar .play-length');
+const progress = $('#progress');
 
 progress.oninput=function(){
     var value = (this.value-this.min)/(this.max-this.min)*100
@@ -26,6 +40,13 @@ progress.oninput=function(){
 const app={
     currentIndex:0,
     songs:[
+        {
+            name:'Stay With Me',
+            singer:'CHANYEOL, PUNCH',
+            path:'./assets/songs/stay-with-me.mp3',
+            image:'./assets/songs/avt_gong-yoo.jpg',
+            duration:192
+        },
         {
             name:'Dangerously',
             singer:'Charlie Puth',
@@ -39,6 +60,13 @@ const app={
             path:'./assets/songs/only.mp3',
             image:'./assets/songs/avt_lee-hi.jpg',
             duration:330
+        },
+        {
+            name:'Before You Go',
+            singer:'Lewis Capaldi',
+            path:'./assets/songs/before-you-go.mp3',
+            image:'./assets/songs/avt_lewis-capaldi.jpg',
+            duration:211
         },
         {
             name:'This is what you came for',
@@ -56,7 +84,7 @@ const app={
         },
         {
             name:'Dandelions',
-            singer:'Lee Hi',
+            singer:'Ruth B',
             path:'./assets/songs/dandelions.mp3',
             image:'./assets/songs/avt_ruth-b.jpg',
             duration:228
@@ -94,7 +122,10 @@ const app={
                         <p class="song-duration">${secondstoMinutesandSeconds(song.duration)}</p>
                     </div>
                     <div class="song-option">
-                        <i class="fa-regular fa-heart option-icon--heart"></i>
+                        <span class="option-favorites">
+                        <i class="fa-solid fa-heart option-icon--fullheart"></i>
+                        <i class="fa-regular fa-heart option-icon--emptyheart"></i>
+                        </span>
                         <i class="fa-solid fa-ellipsis-vertical option-icon--more"></i>
                     </div>
                 </div>
@@ -110,8 +141,10 @@ const app={
         });
     },
     handleEvents(){
+        const _this=this;
         const cd=$('.cd');
         let listSongs=Array.from(playList.children);
+
         // Xu ly khi Scroll Play List
         const cdWidth=cd.offsetWidth;
         const playListHeight=playList.offsetHeight;
@@ -121,25 +154,112 @@ const app={
             const newListHeight=playListHeight+scrollTop;
             cd.style.width= newCdWidth > 50 ? newCdWidth+'px': 0;
             cd.style.opacity=newCdWidth/cdWidth;
-            playList.style.maxHeight=newCdWidth<400? newListHeight+'px':playListHeight+'px';
+            playList.style.maxHeight=newListHeight<400 ? newListHeight+'px':400+'px';
         }
 
-        // Xu ly khi click Play/Pause song
+        // Xu ly CD quay/dung 
+        const cdThumbAnimate = cdThumb.animate([{
+            transform: "rotate(0)"
+            },
+            {
+            transform: "rotate(360deg)"}],{
+                duration:15000,
+                iterations:Infinity
+            }
+        );
+        cdThumbAnimate.pause();
+
+        // Xu ly khi them song vao list yeu thich
+        listSongs.forEach((songItem,index) => {
+            const optionFavorites = songItem.querySelector('.song-option .option-favorites');
+
+            optionFavorites.onclick=function(event){
+                if(this.classList.contains("active")){
+                    this.classList.remove("active");
+                }else{
+                    this.classList.add("active");
+                }
+                event.stopPropagation();
+            }
+        });
+        
+
+        // Xu ly khi click Play/Pause Button
         toggleAudio.onclick=function(){
                 if(player.classList.contains("playing")){
                     audio.pause();
-                    player.classList.remove("playing");
                 } else{
                     audio.play();
-                    player.classList.add("playing");
                 }
         }
+        audio.onplay=function(){
+            player.classList.add("playing");
+            cdThumbAnimate.play();
+        }
+        audio.onpause=function(){
+            player.classList.remove("playing");
+            cdThumbAnimate.pause(); 
 
-        //Xu ly thoi gian khi Play song
+        }
+        // Xu ly khi click Repeat Button
+        repeatAudio.onclick=function(){
+            if(this.classList.contains("active")){
+                this.classList.remove("active");
+                audio.loop=false;
+            }else{
+                this.classList.add("active");
+                audio.loop=true;
+            }
+        }
+        
+        // Xu ly khi click Previous Button
+        preAudio.onclick=function(){
+            if(audio.currentTime<2){
+                listSongs[_this.currentIndex].classList.remove('active');
+                _this.currentIndex=(_this.currentIndex-1 + _this.songs.length)%_this.songs.length;
+                _this.loadSong();
+                audio.play();
+            }else {
+                audio.currentTime=0;
+            }
+        }
+
+        // Xu ly khi click Next Button
+        nextAudio.onclick=function(){
+            listSongs[_this.currentIndex].classList.remove('active');
+            _this.currentIndex=(_this.currentIndex+1)%_this.songs.length;
+            _this.loadSong();
+            audio.play();
+        }
+
+        // Xu ly khi click Random Button
+        randomAudio.onclick=function(){
+            if(this.classList.contains("active")){
+                this.classList.remove("active"); 
+            }else{
+                this.classList.add("active");
+            }
+        }
+        audio.onended=function(){
+            // 2s sau khi ket thuc, tu chuyen qua bai moi
+            setTimeout(function(){
+                listSongs[_this.currentIndex].classList.remove('active');
+                if(randomAudio.classList.contains("active")){
+                    _this.currentIndex=Math.floor(Math.random()*(_this.songs.length+1));
+                } else {
+                    _this.currentIndex=(_this.currentIndex+1)%_this.songs.length;
+                }
+                _this.loadSong();
+                audio.play();
+            },2000);
+            
+        }
+
+        //Xu ly thanh thoi gian khi Play song
         audio.ontimeupdate = function() {
            if(audio.duration){
             playBarCurrent.textContent=secondstoMinutesandSeconds(audio.currentTime);
-            const progessPercent=Math.floor((audio.currentTime/audio.duration)*100);
+            const progessPercent=Math.round((audio.currentTime/audio.duration)*100);
             progress.style.background='linear-gradient(to right, #d8503e 0%, #d8503e ' + progessPercent + '%, #3c4e71 ' + progessPercent + '%, #3c4e71 100%)'
             progress.value=progessPercent;
            }
@@ -153,9 +273,15 @@ const app={
                 songItem.classList.add('active');
                 this.loadSong();
                 audio.play();
-                player.classList.add("playing");
             }
         });
+
+        // Xu ly khi tua nhac tren thanh thoi gian
+        progress.onchange=function(){
+            const progessPercent=(this.value-this.min)/(this.max-this.min);
+            audio.currentTime=audio.duration*progessPercent;
+            audio.play();
+        }
     },
     loadSong(){
         let hasChooseSong=this.currentSong;
